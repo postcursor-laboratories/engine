98e34fa520e029c827dadda7ba99593dedd65030
Charlie Green <geek0795@gmail.com>
2014-06-13 20:32:21 -0700

    ADMIRE THE BEAUTIFULLY LIT GREY CUBE SPIN IN ALL ITS UNPARALLELED SPLENDOR

--- src/Test3DView.cpp
+++ src/Test3DView.cpp
@@ -24,63 +24,6 @@ void Test3DView::draw(sf::RenderTarget*rt){
     //float cutoff[] = { 45 };
     //glLightfv(GL_LIGHT0, GL_SPOT_CUTOFF, cutoff);
 
-    // Enter box coordinates
-    glPushMatrix();    
-    glRotatef(clock.getElapsedTime().asSeconds()*50, 1, 0, 0);
-    glRotatef(clock.getElapsedTime().asSeconds()*30, 0, 1, 0);
-    glRotatef(clock.getElapsedTime().asSeconds()*90, 0, 0, 1);
-    
-    glBegin(GL_QUADS);
-    {
-	float size = 50;
-
-	// BLUE
-	glColor3f(0,0,1);
-	glVertex3f(-size,-size,-size);
-	glVertex3f(-size, size,-size);
-	glVertex3f( size, size,-size);
-	glVertex3f( size,-size,-size);
-
-	// GREEN
-	glColor3f(0,1,0);
-	glVertex3f( size,-size, size);
-	glVertex3f( size, size, size);
-	glVertex3f(-size, size, size);
-	glVertex3f(-size,-size, size);
-
-	// BLUE-GREEN
-	glColor3f(0,1,1);
-	glVertex3f(-size,-size, size);
-	glVertex3f(-size, size, size);
-	glVertex3f(-size, size,-size);
-	glVertex3f(-size,-size,-size);
-
-	// RED
-	glColor3f(1,0,0);
-	glVertex3f( size,-size,-size);
-	glVertex3f( size, size,-size);
-	glVertex3f( size, size, size);
-	glVertex3f( size,-size, size);
-
-	// PURPLE
-	glColor3f(1,0,1);
-	glVertex3f(-size,-size, size);
-	glVertex3f(-size,-size,-size);
-	glVertex3f( size,-size,-size);
-	glVertex3f( size,-size, size);
-
-	// YELLOW
-	glColor3f(1,1,0);
-	glVertex3f( size, size, size);
-	glVertex3f( size, size,-size);
-	glVertex3f(-size, size,-size);
-	glVertex3f(-size, size, size);
-    }
-    glEnd();
-
-    // Leave box coordinates
-    glPopMatrix();
-
     // Draw axes.
     glBegin(GL_LINES);
     {
@@ -97,7 +40,72 @@ void Test3DView::draw(sf::RenderTarget*rt){
 	glVertex3f(0,   0, 100);
     }
     glEnd();
+    
+    // ----------------------------------------------------------------
+    // Enter box coordinates
+    glPushMatrix();    
+    glRotatef(clock.getElapsedTime().asSeconds()*50, 1, 0, 0);
+    glRotatef(clock.getElapsedTime().asSeconds()*30, 0, 1, 0);
+    glRotatef(clock.getElapsedTime().asSeconds()*90, 0, 0, 1);
+    
+    glBegin(GL_QUADS);
+    {
+	float size = 50;
 
+	// BLUE
+	glColor3f(0,0,1);
+	glNormal3f(0,0,-1);
+	glVertex3f(-size,-size,-size);
+	glVertex3f(-size, size,-size);
+	glVertex3f( size, size,-size);
+	glVertex3f( size,-size,-size);
+
+	// GREEN
+	glColor3f(0,1,0);
+	glNormal3f(0,0,1);
+	glVertex3f( size,-size, size);
+	glVertex3f( size, size, size);
+	glVertex3f(-size, size, size);
+	glVertex3f(-size,-size, size);
+
+	// BLUE-GREEN
+	glColor3f(0,1,1);
+	glNormal3d(-1,0,0);
+	glVertex3f(-size,-size, size);
+	glVertex3f(-size, size, size);
+	glVertex3f(-size, size,-size);
+	glVertex3f(-size,-size,-size);
+
+	// RED
+	glColor3f(1,0,0);
+	glNormal3d(1,0,0);
+	glVertex3f( size,-size,-size);
+	glVertex3f( size, size,-size);
+	glVertex3f( size, size, size);
+	glVertex3f( size,-size, size);
+
+	// PURPLE
+	glColor3f(1,0,1);
+	glNormal3d(0,-1,0);
+	glVertex3f(-size,-size, size);
+	glVertex3f(-size,-size,-size);
+	glVertex3f( size,-size,-size);
+	glVertex3f( size,-size, size);
+
+	// YELLOW
+	glColor3f(1,1,0);
+	glNormal3d(0,1,0);
+	glVertex3f( size, size, size);
+	glVertex3f( size, size,-size);
+	glVertex3f(-size, size,-size);
+	glVertex3f(-size, size, size);
+    }
+    glEnd();
+
+    // Leave box coordinates
+    glPopMatrix();
+    // ---------------------------------------------------------------------
+    
     glFlush();
 }
 
