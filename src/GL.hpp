6dd8bbf9d8e2cedefe547cc37fa5d1ebc989fcde
Charlie Green <geek0795@gmail.com>
2014-08-16 17:40:05 -0700

    Updated some comments

--- src/GL.hpp
+++ src/GL.hpp
@@ -1,19 +1,19 @@
 #ifndef GLM_H
 #define GLM_H
 
-// ================================ include GLEW
+// ================================ include GLEW and SFML OpenGL
 #include <GL/glxew.h>
 #include <GL/glew.h>
 
 #include <SFML/OpenGL.hpp>
 
 // ================================ include GLM
-#include <glm/glm.hpp>
 #define GLM_FORCE_RADIANS
+#include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtx/rotate_vector.hpp>
 
-// ================================ define GLM macros
+// // ================================ define GLM macros
 #define Mat4 glm::detail::tmat4x4<double,glm::highp>
 #define Mat3 glm::detail::tmat3x3<double,glm::highp>
 #define Vec3 glm::detail::tvec3<double,glm::highp>
